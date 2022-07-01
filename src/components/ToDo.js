8576089571777b5e62f754621ed9f602d1f8d3a9
shawon-MG <work.shawomg@gmail.com>
2022-07-01 19:23:04 +0600

    Edit button added with new design

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -52,9 +52,8 @@ const ToDo = () => {
                                     <div class="card-actions justify-end">
 
                                         <p className='font-bold'> {task.taskItem} </p>
-                                        <button class="btn btn-square btn-primary bg-gradient-to-r from-orange-400 to-yellow-500 hover:from-green-500 hover:to-blue-500 btn-sm">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                                        </button>
+
+                                        <button class="btn btn-xs btn-primary bg-gradient-to-r from-orange-400 to-yellow-500 hover:from-green-500 hover:to-blue-500"> <span className='text-bold'> Edit Task </span> </button>
 
                                     </div>
                                 </div>
@@ -65,7 +64,7 @@ const ToDo = () => {
             </div>
 
 
-        </div>
+        </div >
 
     );
 };
